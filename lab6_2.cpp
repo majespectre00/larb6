5062786333fcc94d41b49bd8c8993de6be66c335
majespectre00 <oat601_12@hotmail.com>
2020-12-25 16:40:30 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,31 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name, mov, day;
+    int id;
+   cout <<"Fahsai: Sawadee ka...Can you tell me your name?\n" ;
+   cout <<"?????: ";
+   getline(cin,name);
+   cout <<"Fahsai: Wow!!! "<<name<<" is a really cool name.\n";
+   cout <<"Fahsai: I think you are an Engineering student. What is your student ID?\n";
+   cout <<name<<": ";
+   cin>>id;
+   cin.ignore();
+   cout <<"Fahsai: I think you may be GEAR "<<(id/10000000)-12<<". I have a free movie ticket for you.";
+   cout <<"\nFahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n";
+   cout <<name<<": ";
+   getline(cin,mov);
+   cout <<"Fahsai: So....which day are you free to go with me?\n";
+   cout <<name<<": ";
+   getline(cin,day);
+   cout <<"Fahsai: "<<day<<"....that is OK!!! I'm looking forward to watching "<<mov<<" with you.\n";
+   cout <<name<<": ";
+   cin >> mov;
+   cout <<"Fahsai: 555+ see you "<<day<<". Bye Bye \\(^ ^)/";
+}
